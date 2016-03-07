4d18e66594076ff5c7b4cb49b3f786b2af364468
duhao <duhao@weizoom.com>
2016-03-07 11:36:56 +0800

    watchdog采用logging方案，直接输出到终端，取消持久化到mysql和mongo的方案

--- eaglet/core/watchdog/utils.py
+++ eaglet/core/watchdog/utils.py
@@ -1,100 +1,64 @@
 # -*- coding: utf-8 -*-
 
-#coding:utf8
-"""@package weapp.watchdog.utils
+"""@package eaglet.core.watchdog.utils
 
 Watchdog接口
 需要settings里面配置SERVICE_NAME和设置logging格式
 logging.basicConfig(
-	format='%(servicename)s %(type)s %(process)s %(asctime)s %(levelname)s %(message)s', 
+	format='[%(asctime)s] %(name)s %(levelname)s %(message)s', 
 	datefmt="%Y-%m-%d %H:%M:%S", 
 	level=logging.INFO
 )
 """
-
+__author__ = 'duhao'
 import settings
 
-__author__ = 'duhao'
-SERVICE_NAME = settings.SERVICE_NAME
+DEBUG = 1
+INFO = 2
+WARNING = 3
+ERROR = 4
+ALERT = 5
+logger = logging.getLogger(settings.SERVICE_NAME)
 
-def watchdog_debug(message, type='WEB', user_id='0'):
+def watchdog_debug(message, log_type='WEB', user_id='0'):
+	__watchdog(DEBUG, message, log_type, user_id='0')
+
+
+def watchdog_info(message, log_type='WEB', user_id='0'):
+	__watchdog(INFO, message, log_type, user_id='0')
+
+
+def watchdog_warning(message, log_type='WEB', user_id='0'):
+	__watchdog(WARNING, message, log_type, user_id='0')
+
+
+def watchdog_error(message, log_type='WEB', user_id='0'):
+	__watchdog(ERROR, message, log_type, user_id='0')
+
+
+def watchdog_alert(message, log_type='WEB', user_id='0'):
+	__watchdog(ALERT, message, log_type, user_id='0')
+
+
+def __watchdog(level, message, log_type, user_id='0'):
 	"""
-	用异步方式发watchdog
-
-	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
+	@param[in] level 日志级别
+	@param[in] message 日志信息，通常是json格式
+	@param[in] log_type 日志类型，如WEB, API, H5
+	@param[in] user_id 系统账号的user id，用来追踪是哪个用户的系统中出的问题
 	"""
-	result = None
-	if not celeryconfig.CELERY_ALWAYS_EAGER:
-		if WATCHDOG_DEBUG >= settings.WATCH_DOG_LEVEL:
-			result = send_watchdog.delay(type, message, WATCHDOG_DEBUG, user_id, db_name)
-	else:
-		if WATCHDOG_DEBUG >= settings.WATCH_DOG_LEVEL:
-			_watchdog(type, message, WATCHDOG_DEBUG, user_id, db_name)
-	return result
-	#print message
+	if type(user_id) == int:
+		user_id = str(user_id)
 
-
-def watchdog_info(message, type='WEB', user_id='0'):
-	"""
-	用异步方式发watchdog
-
-	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
-	"""
-	result = None
-	if not celeryconfig.CELERY_ALWAYS_EAGER:
-		if WATCHDOG_INFO >= settings.WATCH_DOG_LEVEL:
-			result = send_watchdog.delay(type, message, WATCHDOG_INFO, user_id, db_name)
-	else:
-		if WATCHDOG_INFO >= settings.WATCH_DOG_LEVEL:
-			_watchdog(type, message, WATCHDOG_INFO, user_id, db_name)	
-	return result
-
-def watchdog_warning(message, type='WEB', user_id='0'):
-	"""
-	用异步方式发watchdog
-
-	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
-	"""
-	result = None
-	if not celeryconfig.CELERY_ALWAYS_EAGER:
-		if WATCHDOG_WARNING>= settings.WATCH_DOG_LEVEL:
-			result = send_watchdog.delay(type, message, WATCHDOG_WARNING, user_id, db_name)
-	else:
-		if WATCHDOG_NOTICE >= settings.WATCH_DOG_LEVEL:
-			_watchdog(type, message, WATCHDOG_NOTICE, user_id, db_name)
-	return result
-
-
-def watchdog_error(message, type='WEB', user_id='0'):
-	result = None
-	if not celeryconfig.CELERY_ALWAYS_EAGER:
-		if WATCHDOG_ERROR >= settings.WATCH_DOG_LEVEL:
-			result = send_watchdog.delay(type, message, WATCHDOG_ERROR, user_id, db_name)
-		if settings.DEBUG and (not noraise):
-			exception_type, value, tb = sys.exc_info()
-			if exception_type:
-				raise exception_type, exception_type(value), sys.exc_info()[2]
-	else:
-		if WATCHDOG_ERROR >= settings.WATCH_DOG_LEVEL:
-			_watchdog(type, message, WATCHDOG_ERROR, user_id, db_name)
-		if settings.DEBUG and (not noraise):
-			exception_type, value, tb = sys.exc_info()
-			if exception_type:
-				raise exception_type, exception_type(value), sys.exc_info()[2]
-	return result
-
-
-def watchdog_alert(message, type='WEB', user_id='0'):
-	result = None
-	if not celeryconfig.CELERY_ALWAYS_EAGER:
-		if WATCHDOG_ALERT >= settings.WATCH_DOG_LEVEL:
-			send_watchdog.delay(type, message, WATCHDOG_ALERT, user_id, db_name)
-	else:
-		if WATCHDOG_ALERT >= settings.WATCH_DOG_LEVEL:
-			_watchdog(type, message, WATCHDOG_ALERT, user_id, db_name)
-	return result
-
-
-def __watchdog(message, type, user_id='0'):
-
-	logging.debug(message)
+	#由于logging的限制，自定义的输出信息都拼装到message里进行打印
+	message = '%s %s %s' % (log_type, user_id, message)
+	if level == DEBUG:
+		logger.debug(message)
+	elif level == INFO:
+		logger.info(message)
+	elif level == WARNING:
+		logger.warn(message)
+	elif level == ERROR:
+		logger.error(message)
+	elif level == ALERT:
+		logger.critical(message)
