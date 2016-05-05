230554377d596490d4d93941ecaa3909d47e2c37
bert <1597594211@qq.com>
2016-05-05 12:04:20 +0800

    fix bug

--- eaglet/middlewares/zipkin_middleware.py
+++ eaglet/middlewares/zipkin_middleware.py
@@ -3,15 +3,15 @@
 import settings
 from eaglet.core.zipkin import zipkin_client
 
-class ZipkinMiddleware(object):
-	"""docstring for ZipkinMiddleware"""
-	def process_request(self, request, response):
+    class ZipkinMiddleware(object):
+    """docstring for ZipkinMiddleware"""
+    def process_request(self, request, response):
 
-		zid = request.params.get('zid', None)
-		zdepth = request.params.get('zdepth', 1)
+        zid = request.params.get('zid', None)
         f_zindex = request.params.get('f_zindex', 0)
-		zipkin_client.zipkinClient = None
-		if zid:
-			zipkin_client.zipkinClient = zipkin_client.ZipkinClient(zid, zdepth, f_zindex)
-			#request.params['zipkin_client'] = zipkin_client.zipkinClient 
-		
+        zdepth = request.params.get('zdepth', 1)
+        zipkin_client.zipkinClient = None
+        if zid:
+            zipkin_client.zipkinClient = zipkin_client.ZipkinClient(zid, zdepth, f_zindex)
+
+            #request.params['zipkin_client'] = zipkin_client.zipkinClient 
