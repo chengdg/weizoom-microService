2a334cf63d3121cf206c9b8de2458ba116a4436e
bert <1597594211@qq.com>
2016-05-12 17:01:27 +0800

    delete mongo log call api

--- eaglet/wapi/log.py
+++ eaglet/wapi/log.py
@@ -6,7 +6,7 @@ import settings
 
 from eaglet.core.service.celery import task
 import json
-from eaglet.wapi.logger.mongo_logger import MongoAPILogger
+#from eaglet.wapi.logger.mongo_logger import MongoAPILogger
 import logging
 from eaglet.core.exceptionutil import full_stack
 
@@ -47,12 +47,13 @@ def wapi_log(self, app, resource, method, params, time_in_s, status=0):
 	"""
 	try:
 		if settings.WAPI_LOGGER_ENABLED:
-			global _wapi_logger
-			if _wapi_logger is None:
-				_wapi_logger = MongoAPILogger()
-			if settings.MODE == 'develop' or settings.MODE == 'test':
-				logging.info("develop mode, called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, param_to_text(params)))
-			return _wapi_logger.log(app, resource, method, morph_params(params), time_in_s, status)
+			#update by bert delete MongAPILogger
+			#global _wapi_logger
+			# if _wapi_logger is None:
+			# 	_wapi_logger = MongoAPILogger()
+			# if settings.MODE == 'develop' or settings.MODE == 'test':
+			logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, param_to_text(params)))
+			#return _wapi_logger.log(app, resource, method, morph_params(params), time_in_s, status)
 		else:
 			logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, param_to_text(params)))
 		return 'OK'
