ffee01301ddbfb33b6cd77ab972e33c7ea79de5d
bert <1597594211@qq.com>
2016-07-12 18:07:04 +0800

    add send_task

---
+++ eaglet/utils/send_task.py
@@ -0,0 +1,43 @@
+#coding: utf8
+"""@package service.utils.send_task
+命令行方式启动Celery task
+
+**用法**：
+
+    from eaglet.core.utils.send_task import send_task
+
+    send_task(queue_name, args)
+
+**说明**：
+
+    <service_name> : 服务(队列名称)的名称。如果在@task中指定name，则为次名字；否则为函数的全名。
+
+**用法举例**：
+    
+    integarl_log = {
+        "member_id":1,
+        "nick_name":"weizoom",
+        "event_type": "好友通过分享链接购买商品",
+        "increase_integral": 100,
+        "current_integral": 200
+    }
+    send_task("services.template_message_integral_service.tasks.service_tempate_message_integral", integarl_log)
+
+"""
+
+# import os
+# import sys
+# import json
+
+from celery.execute import send_task as send_celery_task
+from eaglet.core.service import celeryconfig
+from eaglet.core import watchdog
+from eaglet.core.exceptionutil import unicode_full_stack
+
+def send_task(queue_name, args):
+    try:
+        result = send_celery_task(queue_name, args=[args], queue=queue_name)
+    except:
+        notify_message = u"queue_name:{}, args:{}, cause:\n{}".format(queue_name, args, unicode_full_stack())
+        watchdog.error(notify_message)
+    
