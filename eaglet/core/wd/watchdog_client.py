3542054da3e6d695547254be649e87dbf363f2f0
朱天琦 <zhutianqi@weizoom.com>
2017-03-06 18:02:13 +0800

    update

--- eaglet/core/wd/watchdog_client.py
+++ eaglet/core/wd/watchdog_client.py
@@ -57,7 +57,7 @@ class WatchdogClient(object):
 		log = json.dumps({
 			'json_log': message,
 			# 'pson_log': pson.dumps(message, stringify=False)
-			'pson_log': pson.dumps(message, stringify=False)
+			'pson_log': pson.dumps(message)
 		})
 
 		return log
